590fac44a76af3ffc57b4feb2e57ef9ffe07d104
terryshek <terry_shekhinshing@msn.com>
2015-08-07 17:21:46 +0800

    update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4040);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
